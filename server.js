ea30365599f25d016309477a1fafa3af9a59645d
Glitch (bigcooki3-paulhansa-site) <none>
2019-01-08 02:47:31 +0000

    🐛🍈 Checkpoint ./public/css/style.less:876534/45 ./package.json:876534/76 ./style.less:876534/66 ./index.html:876534/87 ./server.js:876534/210

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
