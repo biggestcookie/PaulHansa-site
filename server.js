db8599805994e962b212962ca35f36b4af8583e1
Glitch (bigcooki3-paulhansa-site) <none>
2019-02-04 02:03:12 +0000

    remove public folder

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.use('/public', express.static(__dirname + '/public'));
+app.use('/', express.static(__dirname));
 
 app.get('/', function(request, response) {
   response.sendFile(__dirname + '/index.html');
